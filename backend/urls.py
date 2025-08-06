90fe645b9e9030b4358496398898354d7a8d6ceb
Achuzz2004 <125375888+Achuzz2004@users.noreply.github.com>
2025-08-06 12:16:51 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -33,3 +33,6 @@ if getenv('STORAGE') != 'AWS_S3':
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
